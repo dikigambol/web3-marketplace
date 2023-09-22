c4f61ef2ebf784892c577f13b39468249e70a9c3
dikigambol <westlakewetland@gmail.com>
2023-09-22 15:46:54 +0700

    put marketlist on marketpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import MarketList from "@/components/MarketListings";
 import PromoHero from "@/components/PromoHero";
 
 export default function Home() {
   return (
     <>
       <PromoHero />
-      <MarketList />
     </>
   );
 }
